7d1fa6e5aeb57ac6514df1d53dfa68b3dbdd8372
zhanghx <zhanghx@mangatoon.mobi>
2022-01-17 19:38:42 +0800

    Fix 一些Bug

--- matrix/matrix-android/matrix-apk-canary/src/main/java/com/tencent/matrix/apk/model/task/FindNonAlphaPngTask.java
+++ matrix/matrix-android/matrix-apk-canary/src/main/java/com/tencent/matrix/apk/model/task/FindNonAlphaPngTask.java
@@ -30,6 +30,7 @@ import com.tencent.matrix.apk.model.result.TaskResultFactory;
 import com.tencent.matrix.javalib.util.Log;
 import com.tencent.matrix.javalib.util.Util;
 import com.android.utils.Pair;
+
 import java.awt.image.BufferedImage;
 
 import java.io.File;
@@ -50,116 +51,112 @@ import static com.tencent.matrix.apk.model.task.TaskFactory.TASK_TYPE_FIND_NON_A
 
 public class FindNonAlphaPngTask extends ApkTask {
 
-    private static final String TAG = "Matrix.FindNonAlphaPngTask";
+  private static final String TAG = "Matrix.FindNonAlphaPngTask";
 
-    private File inputFile;
-    private List<Pair<String, Long>> nonAlphaPngList;
-    private long downLimitSize;
-    private Map<String, Pair<Long, Long>> entrySizeMap;
-    private Map<String, String> entryNameMap;
+  private File inputFile;
+  private List<Pair<String, Long>> nonAlphaPngList;
+  private long downLimitSize;
+  private Map<String, Pair<Long, Long>> entrySizeMap;
+  private Map<String, String> entryNameMap;
 
-    public FindNonAlphaPngTask(JobConfig config, Map<String, String> params) {
-        super(config, params);
-        type = TASK_TYPE_FIND_NON_ALPHA_PNG;
+  public FindNonAlphaPngTask(JobConfig config, Map<String, String> params) {
+    super(config, params);
+    type = TASK_TYPE_FIND_NON_ALPHA_PNG;
+  }
+
+  @Override
+  public void init() throws TaskInitException {
+    super.init();
+    String inputPath = config.getUnzipPath();
+    if (Util.isNullOrNil(inputPath)) {
+      throw new TaskInitException(TAG + "---APK-UNZIP-PATH can not be null!");
     }
-
-    @Override
-    public void init() throws TaskInitException {
-        super.init();
-        String inputPath = config.getUnzipPath();
-        if (Util.isNullOrNil(inputPath)) {
-            throw new TaskInitException(TAG + "---APK-UNZIP-PATH can not be null!");
-        }
-        inputFile = new File(inputPath);
-        if (!inputFile.exists()) {
-            throw new TaskInitException(TAG + "---APK-UNZIP-PATH '" + inputPath + "'is not exists!");
-        } else if (!inputFile.isDirectory()) {
-            throw new TaskInitException(TAG + "---APK-UNZIP-PATH '" + inputPath + "' is not directory!");
-        }
-        if (params.containsKey(JobConstants.PARAM_MIN_SIZE_IN_KB)) {
-            try {
-                downLimitSize = Long.parseLong(params.get(JobConstants.PARAM_MIN_SIZE_IN_KB));
-            } catch (NumberFormatException e) {
-                Log.e(TAG, "DOWN-LIMIT-SIZE '" + params.get(JobConstants.PARAM_MIN_SIZE_IN_KB) + "' is not number format!");
-            }
-        }
-        nonAlphaPngList = new ArrayList<Pair<String, Long>>();
-        entrySizeMap = config.getEntrySizeMap();
-        entryNameMap = config.getEntryNameMap();
+    inputFile = new File(inputPath);
+    if (!inputFile.exists()) {
+      throw new TaskInitException(TAG + "---APK-UNZIP-PATH '" + inputPath + "'is not exists!");
+    } else if (!inputFile.isDirectory()) {
+      throw new TaskInitException(TAG + "---APK-UNZIP-PATH '" + inputPath + "' is not directory!");
     }
-
-    private void findNonAlphaPng(File file) throws IOException {
-        if (file != null) {
-            if (file.isDirectory()) {
-                File[] files = file.listFiles();
-                for (File tempFile : files) {
-                    findNonAlphaPng(tempFile);
-                }
-            } else if (file.isFile() && file.getName().endsWith(ApkConstants.PNG_FILE_SUFFIX) && !file.getName().endsWith(ApkConstants.NINE_PNG)) {
-                BufferedImage bufferedImage = ImageIO.read(file);
-                if (bufferedImage != null && bufferedImage.getColorModel() != null && !bufferedImage.getColorModel().hasAlpha()) {
-                    String filename = file.getAbsolutePath().substring(inputFile.getAbsolutePath().length() + 1);
-                    if (entryNameMap.containsKey(filename)) {
-                        filename = entryNameMap.get(filename);
-                    }
-                    long size = file.length();
-                    if (entrySizeMap.containsKey(filename)) {
-                        size = entrySizeMap.get(filename).getFirst();
-                    }
-                    if (size >= downLimitSize * ApkConstants.K1024) {
-                        nonAlphaPngList.add(Pair.of(filename, file.length()));
-                    }
-                }
-            }
-        }
+    if (params.containsKey(JobConstants.PARAM_MIN_SIZE_IN_KB)) {
+      try {
+        downLimitSize = Long.parseLong(params.get(JobConstants.PARAM_MIN_SIZE_IN_KB));
+      } catch (NumberFormatException e) {
+        Log.e(TAG, "DOWN-LIMIT-SIZE '" + params.get(JobConstants.PARAM_MIN_SIZE_IN_KB) + "' is not number format!");
+      }
     }
+    nonAlphaPngList = new ArrayList<Pair<String, Long>>();
+    entrySizeMap = config.getEntrySizeMap();
+    entryNameMap = config.getEntryNameMap();
+  }
 
-    @Override
-    public TaskResult call() throws TaskExecuteException {
-        File resDir = new File(inputFile, ApkConstants.RESOURCE_DIR_PROGUARD_NAME);
-        TaskResult taskResult = null;
+  private void findNonAlphaPng(File file) throws IOException {
+    if (file != null) {
+      if (file.isDirectory()) {
+        File[] files = file.listFiles();
+        for (File tempFile : files) {
+          findNonAlphaPng(tempFile);
+        }
+      } else if (file.isFile() && file.getName().endsWith(ApkConstants.PNG_FILE_SUFFIX) && !file.getName().endsWith(ApkConstants.NINE_PNG)) {
+        BufferedImage bufferedImage = null;
         try {
-            taskResult = TaskResultFactory.factory(getType(), TaskResultFactory.TASK_RESULT_TYPE_JSON, config);
-            long startTime = System.currentTimeMillis();
-            if (resDir.exists() && resDir.isDirectory()) {
-                findNonAlphaPng(resDir);
-            } else {
-                resDir = new File(inputFile, ApkConstants.RESOURCE_DIR_NAME);
-                if (resDir.exists() && resDir.isDirectory()) {
-                    findNonAlphaPng(resDir);
-                }
-            }
-
-            Collections.sort(nonAlphaPngList, new Comparator<Pair<String, Long>>() {
-                @Override
-                public int compare(Pair<String, Long> entry1, Pair<String, Long> entry2) {
-                    long file1Len = entry1.getSecond();
-                    long file2Len = entry2.getSecond();
-                    if (file1Len < file2Len) {
-                        return 1;
-                    } else if (file1Len > file2Len) {
-                        return -1;
-                    } else {
-                        return 0;
-                    }
-                }
-            });
-
-            JsonArray jsonArray = new JsonArray();
-            for (Pair<String, Long> entry : nonAlphaPngList) {
-                if (!Util.isNullOrNil(entry.getFirst())) {
-                    JsonObject jsonObject = new JsonObject();
-                    jsonObject.addProperty("entry-name", entry.getFirst());
-                    jsonObject.addProperty("entry-size", entry.getSecond());
-                    jsonArray.add(jsonObject);
-                }
-            }
-            ((TaskJsonResult) taskResult).add("files", jsonArray);
-            taskResult.setStartTime(startTime);
-            taskResult.setEndTime(System.currentTimeMillis());
-            return taskResult;
+          bufferedImage = ImageIO.read(file);
         } catch (Exception e) {
-            throw new TaskExecuteException(e.getMessage(), e);
+          Log.e(TAG, "findNonAlphaPng exception " + e.getMessage());
         }
+        if (bufferedImage != null && bufferedImage.getColorModel() != null && !bufferedImage.getColorModel().hasAlpha()) {
+          String filename = file.getAbsolutePath().substring(inputFile.getAbsolutePath().length() + 1);
+          if (entryNameMap.containsKey(filename)) {
+            filename = entryNameMap.get(filename);
+          }
+          long size = file.length();
+          if (entrySizeMap.containsKey(filename)) {
+            size = entrySizeMap.get(filename).getFirst();
+          }
+          if (size >= downLimitSize * ApkConstants.K1024) {
+            nonAlphaPngList.add(Pair.of(filename, file.length()));
+          }
+        }
+      }
     }
+  }
+
+  @Override
+  public TaskResult call() throws TaskExecuteException {
+    File resDir = new File(inputFile, ApkConstants.RESOURCE_DIR_PROGUARD_NAME);
+    TaskResult taskResult = null;
+    try {
+      taskResult = TaskResultFactory.factory(getType(), TaskResultFactory.TASK_RESULT_TYPE_JSON, config);
+      long startTime = System.currentTimeMillis();
+      if (resDir.exists() && resDir.isDirectory()) {
+        findNonAlphaPng(resDir);
+      } else {
+        resDir = new File(inputFile, ApkConstants.RESOURCE_DIR_NAME);
+        if (resDir.exists() && resDir.isDirectory()) {
+          findNonAlphaPng(resDir);
+        }
+      }
+
+      nonAlphaPngList.sort((entry1, entry2) -> {
+        long file1Len = entry1.getSecond();
+        long file2Len = entry2.getSecond();
+        return Long.compare(file2Len, file1Len);
+      });
+
+      JsonArray jsonArray = new JsonArray();
+      for (Pair<String, Long> entry : nonAlphaPngList) {
+        if (!Util.isNullOrNil(entry.getFirst())) {
+          JsonObject jsonObject = new JsonObject();
+          jsonObject.addProperty("entry-name", entry.getFirst());
+          jsonObject.addProperty("entry-size", entry.getSecond());
+          jsonArray.add(jsonObject);
+        }
+      }
+      ((TaskJsonResult) taskResult).add("files", jsonArray);
+      taskResult.setStartTime(startTime);
+      taskResult.setEndTime(System.currentTimeMillis());
+      return taskResult;
+    } catch (Exception e) {
+      throw new TaskExecuteException(e.getMessage(), e);
+    }
+  }
 }
--- matrix/matrix-android/matrix-apk-canary/src/main/java/com/tencent/matrix/apk/model/task/util/ManifestParser.java
+++ matrix/matrix-android/matrix-apk-canary/src/main/java/com/tencent/matrix/apk/model/task/util/ManifestParser.java
@@ -22,7 +22,9 @@ import brut.androlib.res.decoder.AXmlResourceParser;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.tencent.matrix.javalib.util.Util;
+
 import org.xmlpull.v1.XmlPullParser;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -34,108 +36,110 @@ import java.util.Stack;
 
 public class ManifestParser {
 
-    private static final String ROOTTAG = "manifest";
-    private final AXmlResourceParser resourceParser;
-    private File manifestFile;
-    private boolean isParseStarted;
-    private final Stack<JsonObject> jsonStack = new Stack<>();
-    private JsonObject result;
+  private static final String ROOTTAG = "manifest";
+  private final AXmlResourceParser resourceParser;
+  private File manifestFile;
+  private boolean isParseStarted;
+  private final Stack<JsonObject> jsonStack = new Stack<>();
+  private JsonObject result;
 
-    public ManifestParser(String path) {
-        manifestFile = new File(path);
-        resourceParser = ApkResourceDecoder.createAXmlParser();
+  public ManifestParser(String path) {
+    manifestFile = new File(path);
+    resourceParser = ApkResourceDecoder.createAXmlParser();
+  }
+
+  public ManifestParser(File manifestFile) {
+    if (manifestFile != null) {
+      this.manifestFile = manifestFile;
     }
+    resourceParser = ApkResourceDecoder.createAXmlParser();
+  }
 
-    public ManifestParser(File manifestFile) {
-        if (manifestFile != null) {
-            this.manifestFile = manifestFile;
+  public ManifestParser(File manifestFile, File arscFile) throws IOException, AndrolibException {
+    if (manifestFile != null) {
+      this.manifestFile = manifestFile;
+    }
+    resourceParser = ApkResourceDecoder.createAXmlParser(arscFile);
+  }
+
+  public JsonObject parse() throws Exception {
+
+    FileInputStream inputStream = null;
+    try {
+      inputStream = new FileInputStream(manifestFile);
+      try {
+        resourceParser.open(inputStream);
+        int token = resourceParser.nextToken();
+
+        while (token != XmlPullParser.END_DOCUMENT) {
+          token = resourceParser.next();
+          if (token == XmlPullParser.START_TAG) {
+            handleStartElement();
+          } else if (token == XmlPullParser.TEXT) {
+            handleElementContent();
+          } else if (token == XmlPullParser.END_TAG) {
+            handleEndElement();
+          }
         }
-        resourceParser = ApkResourceDecoder.createAXmlParser();
-    }
-
-    public ManifestParser(File manifestFile, File arscFile) throws IOException, AndrolibException {
-        if (manifestFile != null) {
-            this.manifestFile = manifestFile;
+      } finally {
+        resourceParser.close();
+        if (inputStream != null) {
+          inputStream.close();
         }
-        resourceParser = ApkResourceDecoder.createAXmlParser(arscFile);
-    }
-
-    public JsonObject parse() throws Exception {
-
-        FileInputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(manifestFile);
-            try {
-                resourceParser.open(inputStream);
-                int token = resourceParser.nextToken();
-
-                while (token != XmlPullParser.END_DOCUMENT) {
-                    token = resourceParser.next();
-                    if (token == XmlPullParser.START_TAG) {
-                        handleStartElement();
-                    } else if (token == XmlPullParser.TEXT) {
-                        handleElementContent();
-                    } else if (token == XmlPullParser.END_TAG) {
-                        handleEndElement();
-                    }
-                }
-            } finally {
-                resourceParser.close();
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-            }
-        } catch (Exception e) {
-            throw e;
-        }
-
-
-        return result;
+      }
+    } catch (Exception e) {
+      throw e;
     }
 
 
-    private void handleStartElement() {
-        final String name = resourceParser.getName();
+    return result;
+  }
 
-        if (name.equals(ROOTTAG)) {
-            isParseStarted = true;
-        }
-        if (isParseStarted) {
-            JsonObject jsonObject = new JsonObject();
-            for (int i = 0; i < resourceParser.getAttributeCount(); i++) {
-                if (!Util.isNullOrNil(resourceParser.getAttributePrefix(i))) {
-                    jsonObject.addProperty(resourceParser.getAttributePrefix(i) + ":" + resourceParser.getAttributeName(i), resourceParser.getAttributeValue(i));
-                } else {
-                    jsonObject.addProperty(resourceParser.getAttributeName(i), resourceParser.getAttributeValue(i));
-                }
-            }
-            jsonStack.push(jsonObject);
-        }
+
+  private void handleStartElement() {
+    final String name = resourceParser.getName();
+
+    if (name.equals(ROOTTAG)) {
+      isParseStarted = true;
     }
-
-    private void handleElementContent() {
-        //do nothing
-    }
-
-    private void handleEndElement() {
-        final String name = resourceParser.getName();
-        JsonObject jsonObject = jsonStack.pop();
-
-        if (jsonStack.isEmpty()) {                                      //root element
-            result = jsonObject;
+    if (isParseStarted) {
+      JsonObject jsonObject = new JsonObject();
+      for (int i = 0; i < resourceParser.getAttributeCount(); i++) {
+        if (!Util.isNullOrNil(resourceParser.getAttributePrefix(i))) {
+          jsonObject.addProperty(resourceParser.getAttributePrefix(i) + ":" + resourceParser.getAttributeName(i), resourceParser.getAttributeValue(i));
         } else {
-            JsonObject preObject = jsonStack.peek();
-            JsonArray jsonArray = null;
-            if (preObject.has(name)) {
-                jsonArray = preObject.getAsJsonArray(name);
-                jsonArray.add(jsonObject);
-            } else {
-                jsonArray = new JsonArray();
-                jsonArray.add(jsonObject);
-                preObject.add(name, jsonArray);
-            }
+          jsonObject.addProperty(resourceParser.getAttributeName(i), resourceParser.getAttributeValue(i));
         }
+      }
+      jsonStack.push(jsonObject);
     }
+  }
+
+  private void handleElementContent() {
+    //do nothing
+  }
+
+  private void handleEndElement() {
+    final String name = resourceParser.getName();
+    JsonObject jsonObject = jsonStack.pop();
+
+    if (jsonStack.isEmpty()) {                                      //root element
+      result = jsonObject;
+    } else {
+      JsonObject preObject = jsonStack.peek();
+      JsonArray jsonArray = null;
+      if (preObject.has(name)) {
+        if (preObject.isJsonArray()) {
+          jsonArray = preObject.getAsJsonArray(name);
+          jsonArray.add(jsonObject);
+        }
+      } else {
+        jsonArray = new JsonArray();
+        jsonArray.add(jsonObject);
+        preObject.add(name, jsonArray);
+      }
+    }
+  }
 
 
 }
